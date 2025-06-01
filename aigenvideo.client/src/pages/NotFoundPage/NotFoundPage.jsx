65990be84eec2b7c9b3ef34dcfa6c065a130ac65
mquangcao <mquangcao38@gmail.com>
2025-06-01 12:09:01 +0700

    feat : implement NotFoundPage and ForbiddenPage

--- aigenvideo.client/src/pages/NotFoundPage/NotFoundPage.jsx
+++ aigenvideo.client/src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 function NotFoundPage() {
   return (
-    <div className="flex justify-center items-center h-screen">404 NotFoundPage</div>
-  )
+    <div className="min-h-screen bg-white flex items-center justify-center">
+      <div className="flex items-center space-x-6">
+        <h1 className="text-2xl font-medium text-black">404</h1>
+        <div className="h-12 w-px bg-gray-300"></div>
+        <p className="text-sm text-black">This page could not be found.</p>
+      </div>
+    </div>
+  );
 }
 
-export default NotFoundPage
+export default NotFoundPage;
