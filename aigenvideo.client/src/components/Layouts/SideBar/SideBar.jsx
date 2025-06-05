22ad71082991e9ba66902ac30ab357f9b177d964
Giaphuc713 <phucha2639@gmail.com>
2025-06-05 14:03:18 +0700

    feat: commit before merge

--- aigenvideo.client/src/components/Layouts/SideBar/SideBar.jsx
+++ aigenvideo.client/src/components/Layouts/SideBar/SideBar.jsx
@@ -6,7 +6,7 @@ const SideBar = ({ isOpen, toggleSideBar }) => {
   return (
     <aside className={`bg-slate-700 text-white p-4 space-y-6 transition-all duration-300 ease-in-out ${isOpen ? 'w-64' : 'w-20'}`}>
       {/* Nút đóng/mở Sidebar*/}
-      <div className="flex justify-end mb-4">
+      <div className="flex justify-end mb-4 ">
         <button
           onClick={toggleSideBar}
           className="p-2 rounded-md hover:bg-slate-600"
@@ -18,11 +18,11 @@ const SideBar = ({ isOpen, toggleSideBar }) => {
 
       {/* Phần menu trên */}
       <nav className="space-y-2">
-        <a href="#" className="flex items-center space-x-2 p-2 rounded-md hover:bg-slate-600">
+        <a href="#" className={`flex items-center space-x-2 p-2 rounded-md hover:bg-slate-600 ${!isOpen && 'justify-center'}`}>
           <FaHome size={20} />
           {isOpen && <span>Home</span>}
         </a>
-        <a href="#" className="flex items-center space-x-2 p-2 rounded-md hover:bg-slate-600">
+        <a href="#" className={`flex items-center space-x-2 p-2 rounded-md hover:bg-slate-600 ${!isOpen && 'justify-center'}`}>
           <FaCreditCard size={20} />
           {isOpen && <span>Manage Subscription</span>}
         </a>
@@ -31,7 +31,7 @@ const SideBar = ({ isOpen, toggleSideBar }) => {
       {/* Phần các công cụ AI */}
       <nav className="space-y-2">
         {isOpen && <h3 className="text-xs uppercase text-gray-400 font-semibold pt-4 px-2">AI Tools</h3>}
-        <a href="#" className={`flex items-center space-x-2 p-2 rounded-md ${isOpen ? 'bg-slate-600' : 'bg-slate-600 justify-center'}`}>
+        <a href="#" className={`flex items-center space-x-2 p-2 rounded-md hover:bg-slate-600 ${!isOpen && 'justify-center'}`}>
           <FaRobot size={20} />
           {isOpen && <span>AI Video Generator</span>}
         </a>
