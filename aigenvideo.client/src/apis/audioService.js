42bcc4bbebe105e146e2dbbac4515a34f7e92758
HCMUSTooTired <22120364@student.hcmus.edu.vn>
2025-06-27 11:51:49 +0700

    feat: refactor audio api

---
+++ aigenvideo.client/src/apis/audioService.js
@@ -0,0 +1,23 @@
+﻿import axiosClient from './axiosClient';
+
+/**
+ * Gửi yêu cầu đến backend để tạo âm thanh dựa trên văn bản.
+ * @param {string} text - Văn bản người dùng nhập vào.
+ * @param {string} selectedGoogleVoice - Giọng nói được chọn (e.g., 'en-US-Wavenet-D').
+ * @param {string} languageCode - Mã ngôn ngữ (e.g., 'en-US').
+ * @param {number} speechRate - Tốc độ đọc (e.g., 1.0).
+ * @returns {Promise<object>} Promise trả về phản hồi từ API với dạng blob.
+ */
+export const generateAudio = async (text, selectedGoogleVoice, languageCode, speechRate) => {
+    return await axiosClient({
+        method: 'post',
+        url: '/api/texttospeech/synthesize',
+        data: {
+            text: text,
+            voiceName: selectedGoogleVoice,
+            languageCode: languageCode,
+            speechRate: speechRate,
+        },
+        responseType: 'blob',
+    });
+};
